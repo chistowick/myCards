9e8326f5e53da349b6980dab8f4d777cd927ca20
Anatoly <chistowick@yandex.ru>
2020-06-03 17:16:49 +0300

    Реализовал следующий функционал: 1) Смена карточек вперед/назад по клику. 2) Переворачивание карточки по клику

--- js/showCard.js
+++ js/showCard.js
@@ -1,36 +1,119 @@
 "use strict";
 
-// По клику на активном стеке
-$(document).ready(function () {
-    $('#active_stack').click(function showActiveCard() {
+// На кнопу FORWARD навешиваем обработчик
+window.onload = setButtonsAction('#forward', 'click', showForwardCard);
 
+// На кнопу BACK навешиваем обработчик
+window.onload = setButtonsAction('#back', 'click', showBackCard);
+
+// На активную карточку навешиваем обработчик
+window.onload = setButtonsAction('#active_stack', 'click', upendCard);
+
+function showForwardCard() {
+
+    // Устанавливаем активную сторону карточки по умолчанию
+    $(activeSideId).css({'display': 'block'});
+    $(inactiveSideId).css({'display': 'none'});
+
+    forwardCard();
+    updateCardView();
+}
+
+function showBackCard() {
+
+    // Устанавливаем активную сторону карточки по умолчанию
+    $(activeSideId).css({'display': 'block'});
+    $(inactiveSideId).css({'display': 'none'});
+
+    backCard();
+    updateCardView();
+}
+
+function upendCard() {
+
+    // Если порядковый номер карточки меньше нуля, т.е. только что произошла 
+    // смена стека, то пререходим к последнему элементу массива - т.е. к верхней
+    // карточке, показываем её и ждем дальнейших действий пользователя
+    if (curentNumberActiveCard < 0)
+    {
+        showBackCard();
+        return;
+    }
+
+    let originalSide = $('#original_side').css('display');
+    let translationSide = $('#translation_side').css('display');
+
+    if (originalSide == 'none') {
+        $('#original_side').css({'display': 'block'});
+        $('#translation_side').css({'display': 'none'});
+    } else {
+        $('#original_side').css({'display': 'none'});
+        $('#translation_side').css({'display': 'block'});
+    }
+}
+
+function forwardCard() {
     // Добавляем к существующему номеру текущего элемента массива активных 
     // карточек единицу, таким образом определяем номер новой карточки
     curentNumberActiveCard = curentNumberActiveCard + 1;
 
     // Если номер достиг величины массива, пререходим к началу массива - 
-    // т.е. к первой карточке
-    if (!(curentNumberActiveCard < activeCardsArray.length))
+    // т.е. к нижней карточке
+    if (curentNumberActiveCard >= activeCardsArray.length)
     {
         curentNumberActiveCard = 0;
     }
+}
+
+function backCard() {
+    // Вычитаем из существующего номера текущего элемента массива активных 
+    // карточек единицу, таким образом определяем номер новой карточки
+    curentNumberActiveCard = curentNumberActiveCard - 1;
+
+    // Если порядковый номер стал меньше нуля, пререходим к последнему элементу массива - 
+    // т.е. к верхней карточке
+    if (curentNumberActiveCard < 0)
+    {
+        curentNumberActiveCard = (activeCardsArray.length - 1);
+    }
+}
+
+function updateCardView() {
 
     let k = curentNumberActiveCard;
 
     // Задаем переменные, ссылающиеся на соответствующие данные в объектах-карточках
     let original = activeCardsArray[k].original;
+    let originalComment = activeCardsArray[k].originalComment;
     let translation = activeCardsArray[k].translation;
-    let comment = activeCardsArray[k].comment;
+    let translationComment = activeCardsArray[k].translationComment;
 
     // Очищаем данные предыдущей карточки
     document.querySelector('#original').innerHTML = ``;
+    document.querySelector('#original_comment').innerHTML = ``;
     document.querySelector('#translation').innerHTML = ``;
-    document.querySelector('#comment').innerHTML = ``;
+    document.querySelector('#translation_comment').innerHTML = ``;
 
     // Выводим данные новой карточки
     document.querySelector('#original').insertAdjacentHTML("afterBegin", original);
+    document.querySelector('#original_comment').insertAdjacentHTML("afterBegin", originalComment);
     document.querySelector('#translation').insertAdjacentHTML("afterBegin", translation);
-    document.querySelector('#comment').insertAdjacentHTML("afterBegin", comment);
+    document.querySelector('#translation_comment').insertAdjacentHTML("afterBegin", translationComment);
+}
 
-});
-});
+// Set an event listener for each element. (selector, event, action)
+function setButtonsAction(selector, event, action) {
+
+    let elements = document.querySelectorAll(selector);
+    let oneElement;
+
+    // Iterating through elements
+    for (let j = 0; j < elements.length; j++) {
+
+        oneElement = elements[j];
+
+        // Setting actions
+        oneElement.addEventListener(event, action);
+
+    }
+}
